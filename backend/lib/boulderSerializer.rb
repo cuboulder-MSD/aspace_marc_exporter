197efe0297606765cd74893a619ecf5fb59eb982
University of Colorado Boulder Libraries - Metadata Production & Curation Services <47370990+cuboulder-MSD@users.noreply.github.com>
2024-05-16 16:11:53 -0600

    Update boulderSerializer.rb

--- backend/lib/boulderSerializer.rb
+++ backend/lib/boulderSerializer.rb
@@ -34,7 +34,7 @@ class BoulderMARCSerializer
     # extra_fields << add_024_tag
     # extra_fields << add_035_tag
     extra_fields << add_043_tag
-    extra_fields << add_506_tag
+    # extra_fields << add_506_tag
     extra_fields << add_655_tag
     extra_fields << add_710_tag
     
@@ -159,14 +159,37 @@ class BoulderMARCSerializer
 
 
 
-  def add_506_tag
-    value = "Collection is open for research use."
-    subfields_hsh = {}
-    datafield_hsh = get_datafield_hash('506','0',' ')
-    subfields_hsh[1] = get_subfield_hash('a', value)
-    datafield = BoulderCustomTag.new(datafield_hsh,subfields_hsh)
-    datafield.add_datafield_tag
-  end
+#   def add_506_tag
+#     restrictions_apply = @record.aspace_record['restrictions_apply']
+    
+#     restrictions_text = restrictions_apply ? "Collection is open for research use." : "Collection is restricted."
+#     subfields_hsh = {}
+#     datafield_hsh = get_datafield_hash('506','0',' ')
+#     subfields_hsh[1] = get_subfield_hash('a', restrictions_text)
+#     datafield = BoulderCustomTag.new(datafield_hsh,subfields_hsh)
+#     datafield.add_datafield_tag
+# end
+
+# def add_506_tag
+#   restrictions_apply = @record.aspace_record['restrictions_apply']
+  
+#   if restrictions_apply == 'false'
+#       indicator_value = '1' # If restrictions apply is false, set indicator value to 1
+#       restrictions_text = "Collection is open for research use."
+#   else
+#       indicator_value = '0' # If restrictions apply is true or nil, set indicator value to 0
+#       restrictions_text = "Collection is restricted."
+#   end
+  
+#   subfields_hsh = {}
+#   datafield_hsh = get_datafield_hash('506', indicator_value, ' ')
+#   subfields_hsh[1] = get_subfield_hash('a', restrictions_text)
+  
+#   datafield = BoulderCustomTag.new(datafield_hsh, subfields_hsh)
+#   datafield.add_datafield_tag
+# end
+
+
 
   def add_655_tag
     subfields_hsh = {}
