908bc5f2a3cda57297f91f487d2931a9217908e9
University of Colorado Boulder Libraries - Metadata Production & Curation Services <47370990+cuboulder-MSD@users.noreply.github.com>
2024-05-18 09:41:55 -0600

    Update plugin_init.rb

--- backend/plugin_init.rb
+++ backend/plugin_init.rb
@@ -1,6 +1,6 @@
-require_relative 'model/boulderSerializer'
-require_relative 'model/boulderMarc'
-require_relative 'model/boulderExporter'
+require_relative 'lib/boulderSerializer'
+require_relative 'lib/boulderMarc'
+require_relative 'lib/boulderExporter'
 require_relative 'lib/aspace_extensions'
 
 MARCSerializer.add_decorator(BoulderMARCSerializer)
