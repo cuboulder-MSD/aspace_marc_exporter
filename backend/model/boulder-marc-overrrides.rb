ad150f0c1b433a089149f416e3f15bcc8e97b9a1
University of Colorado Boulder Libraries - Metadata Production & Curation Services <47370990+cuboulder-MSD@users.noreply.github.com>
2024-07-10 17:01:05 -0600

    Update boulder-marc-overrrides.rb

--- backend/model/boulder-marc-overrrides.rb
+++ backend/model/boulder-marc-overrrides.rb
@@ -112,7 +112,7 @@ class MARCModel < ASpaceExport::ExportModel
 
     if ark_url = ark_name['current']
       df('856', '4', '2').with_sfs(
-        ['z', "Finding Aid Online:"],
+        ['z', "Finding Aid (via ArchivesSpace)"],
         ['u', ark_url]
       ) unless ark_url.nil? || ark_url.empty?
 
